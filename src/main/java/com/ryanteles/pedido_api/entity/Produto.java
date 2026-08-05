c5f693aef2b4fbd63f1dc81bf7d1a761de4b841a
Ryan Teles <ryanteles2018@gmail.com>
2026-08-05 15:17:52 -0300

    feat: create Produto entity

---
+++ src/main/java/com/ryanteles/pedido_api/entity/Produto.java
@@ -0,0 +1,31 @@
+package com.ryanteles.pedido_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer estoque;
+}
